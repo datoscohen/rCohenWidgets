14f87425f15a0e82bb57305fd7bf27f0c8f3ea76
Juan P. Dugo <juanpatriciodugo@gmail.com>
2023-05-30 16:33:24 -0300

    se retornan dos valores en setValue

--- app.R
+++ app.R
@@ -14,6 +14,10 @@ server <- function(input, output, session) {
   output$textOutput <- renderText({
     sprintf("You entered: %s", input$textInput)
   })
+
+  observeEvent(input$textInput, {
+    print(input$textInput)
+  })
 }
 
 shinyApp(ui, server)
--- srcjs/strengthBar.jsx
+++ srcjs/strengthBar.jsx
@@ -2,27 +2,43 @@ import { reactShinyInput } from 'reactR';
 import PasswordStrengthBar from 'react-password-strength-bar';
 
 const PasswordInputBar = ({ configuration, value, setValue }) => {
- return (
-  <div>
-    <div class="form-group shiny-input-container"  style={{ width: '100%' }}>
-      <label class="control-label">
-      <i class="fas fa-lock" role="presentation" aria-label="lock icon"></i>
-        &nbsp;Nueva Contraseña
-      </label>
-      <input
-        type="password"
-        class="form-control"
-        value={value}
-        onChange={e => setValue(e.target.value)}
+  const [password, setPassword] = React.useState("");
+  const [newScore, setScore] = React.useState(0);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setValue([e.target.value, newScore]);
+  };
+
+  const handleScoreChange = (score, feedback) => {
+    setScore(score); // update the score state
+    if (score) {
+      setValue([password, score])
+    }
+  };
+
+  return (
+    <div>
+      <div class="form-group shiny-input-container"  style={{ width: '100%' }}>
+        <label class="control-label">
+          <i class="fas fa-lock" role="presentation" aria-label="lock icon"></i>
+          &nbsp;Nueva Contraseña
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <PasswordStrengthBar
+        password={password}
+        scoreWords={configuration.scoreWords}
+        minLength={configuration.minLength}
+        shortScoreWord={configuration.shortScoreWord}
+        onChangeScore={handleScoreChange}
       />
     </div>
-    <PasswordStrengthBar
-      password={value}
-      scoreWords={configuration.scoreWords}
-      minLength={configuration.minLength}
-      shortScoreWord={configuration.shortScoreWord}
-      />
-  </div>
   );
 };
 
